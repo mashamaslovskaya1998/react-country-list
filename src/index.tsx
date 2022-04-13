4e81a995361b0854081677f11783037da9672eca
mashamaslovskaya1998 <mashamaslovskaya1998@mail.ru>
2022-04-13 17:51:54 +0300

    fix(hw-37): correct index

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
-const container = document.getElementById("root") as HTMLElement;
-const root = createRoot(container);
-root.render(<App />);
-
+const container = document.getElementById("root");
 if (!container) {
   throw new Error("No root element found");
 }
+const root = createRoot(container);
+root.render(<App />);
